34f56e54eb593d8b054b674270338b79417e4036
Hebert  F. Barros <mrrabbitomg@gmail.com>
2020-03-25 20:24:56 +0000

    react init to home_page

--- lib/google_map_stub.dart
+++
@@ -1,5 +0,0 @@
-import 'google_map_base.dart';
-
-/// Implemented in `google_map_browser.dart` and `google_map_mobile.dart`.
-BaseGoogleMap getGoogleMap() => throw UnsupportedError(
-    'Cannot create a client without dart:html or dart:io.');
