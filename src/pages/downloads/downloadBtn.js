be6b3f40631541a26a479a3063960378af4513da
tecnobert <mrrabbitomg@gmail.com>
2020-03-26 18:08:18 -0300

    btns shared

---
+++ src/pages/downloads/downloadBtn.js
@@ -0,0 +1,24 @@
+import AirplayIcon from "@material-ui/icons/Airplay";
+import Button from "@material-ui/core/Button";
+import React from "react";
+import { urls } from "../../assets/app.json";
+
+//import Styled from "../../styles/react/styles";
+
+function DownloadBtn() {
+  //const classes = Styled();
+
+  return (
+    <Button
+      variant="contained"
+      color="secondary"
+      target="_blank"
+      rel="noopener noreferrer"
+      href={urls.external[0]}
+    >
+      <AirplayIcon></AirplayIcon> Test app now
+    </Button>
+  );
+}
+
+export default DownloadBtn;
---
+++ src/shared/whoInt.js
@@ -0,0 +1,24 @@
+import Button from "@material-ui/core/Button";
+import LiveHelpIcon from "@material-ui/icons/LiveHelp";
+import React from "react";
+import { urls } from "../assets/app.json";
+
+//import Styled from "../../styles/react/styles";
+
+function WhoIntBtn() {
+  //const classes = Styled();
+
+  return (
+    <Button
+      variant="contained"
+      color="primary"
+      target="_blank"
+      rel="noopener noreferrer"
+      href={urls.external[2]}
+    >
+      <LiveHelpIcon></LiveHelpIcon> Read more in who.int
+    </Button>
+  );
+}
+
+export default WhoIntBtn;
